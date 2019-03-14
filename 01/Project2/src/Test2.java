81607d52ff9683759dc89032ad18c96c48e73f96
KIMYOORIM <kyo9721@naver.com>
2019-03-14 19:55:09 +0900

    .

---
+++ 01/Project2/src/Test2.java
@@ -0,0 +1,34 @@
+
+public class Test2 {
+	public static void main(String[] args) {
+
+		char a = 'A';
+		char b = 'B';
+
+		System.out.print(a);
+		System.out.print('\"');
+		System.out.println(b);
+		
+		char d = 'h';
+		char g = '\t';
+		char j = 'l';
+		System.out.print(d);
+		System.out.print(g);
+		System.out.println(j);
+		
+		char r = 'q';
+		char m = '\n';
+		char v = 'k';
+		char x = '\n';
+		char s = 'p';
+		System.out.print(r);
+		System.out.print(m);
+		System.out.print(v);
+		System.out.print(x);
+		System.out.print(s);
+		
+		
+		
+
+	}
+}
